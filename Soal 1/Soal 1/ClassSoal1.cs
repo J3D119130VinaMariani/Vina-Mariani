ee2bb3578c06e6df514fb45f3b109f361eb0c67d
VinaMariani <bbvinamariani@gmail.com>
2023-02-01 06:18:03 +0700

    test

---
+++ Soal 1/Soal 1/ClassSoal1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soal_1
+{
+    internal class ClassSoal1
+    {
+        public void  Soal1()
+        {
+              for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i%3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i%5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }              
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }  
+        }
+    }
+}
---
+++ Soal 1/Soal 1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Soal_1;
+
+Console.WriteLine("Soal 1 Pemrograman");
+new ClassSoal1().Soal1();
